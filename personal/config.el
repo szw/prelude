66a04badd82b7eeabdc919f66929701636780922
Szymon Wrozynski <szymon@wrozynski.com>
2016-11-06 01:06:58 +0100

    Update settings

--- personal/config.el
+++ personal/config.el
@@ -36,6 +36,7 @@
 (global-set-key (kbd "s-_") 'undo-tree-redo)
 (global-set-key (kbd "s-z") 'zop-up-to-char)
 (global-set-key (kbd "s-q") 'fill-paragraph)
+(global-set-key (kbd "s-s") 'sp-splice-sexp)
 
 (add-hook 'golden-ratio-mode-hook
           (lambda ()
@@ -87,21 +88,3 @@
 
 ;; disable semantic highlighting entirely
 (setq ensime-sem-high-enabled-p nil)
-
-;; turn off implicits underscoring in default Prelude theme
-;; (setq ensime-sem-high-faces
-;;       '((var . scala-font-lock:var-face)
-;;         (val :inherit font-lock-constant-face :slant italic)
-;;         (varField . scala-font-lock:var-face)
-;;         (valField :inherit font-lock-constant-face :slant italic)
-;;         (functionCall . font-lock-function-name-face)
-;;         (operator . font-lock-keyword-face)
-;;         (param :slant italic)
-;;         (class . font-lock-type-face)
-;;         (trait :inherit font-lock-type-face :slant italic)
-;;         (object . font-lock-constant-face)
-;;         (package . font-lock-preprocessor-face)
-;;         (implicitConversion . nil)
-;;         (implicitParams . nil)
-;;         (deprecated :strike-through "dark gray"))
-;;       )
